6d3ac5567aee8caf01f1bee7db23bed94e690397
Jon Mico <jonmico@gmail.com>
2023-04-20 13:08:35 -0400

    break card into own component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Card from '../card/card.component';
 import './card-list.styles.css';
 
 class CardList extends Component {
@@ -10,16 +11,7 @@ class CardList extends Component {
       <div className='card-list'>
         {monsters.map((monster) => {
           const { name, id, email } = monster;
-          return (
-            <div className='card-container' key={id}>
-              <img
-                alt={`monster ${name}`}
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card name={name} id={id} email={email} />;
         })}
       </div>
     );
